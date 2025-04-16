a9b1f11e2b9db08c8f3add9b3cb2459736b9ff3b
PJongHo03 <ghdls1254@gmail.com>
2025-04-16 19:48:20 +0900

    ,

--- src/components/movieSearch/index.jsx
+++ src/components/movieSearch/index.jsx
@@ -17,30 +17,21 @@ function MovieSearch({ searchMovieApi, setMovies }) {
 
   return (
     <main>
-      <div className="search-container">
-        <div className="search-bar">
+      <div className='search-container'>
+        <div className='search-bar'>
           <input
-            type="text"
-            className="search-input"
-            placeholder="영화 제목, 배우, 감독 검색..."
+            type='text'
+            className='search-input'
+            placeholder='영화 제목, 배우, 감독 검색...'
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             onKeyDown={handleKeyDown}
           />
-          <button className="search-button" onClick={handleSearch}>
+          <button className='search-button' onClick={handleSearch}>
             검색
           </button>
         </div>
       </div>
-
-      {/* <div className='filter-section'>
-        <button className='filter-button active'>전체</button>
-        <button className='filter-button'>액션</button>
-        <button className='filter-button'>코미디</button>
-        <button className='filter-button'>로맨스</button>
-        <button className='filter-button'>SF</button>
-        <button className='filter-button'>호러</button>
-      </div> */}
     </main>
   );
 }
